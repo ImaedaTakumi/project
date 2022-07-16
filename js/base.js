89b14dcea03711179ff0a58312f711a137054ec9
Takumi <c0a20021ab@edu.teu.ac.jp>
2022-07-17 04:03:21 +0900

    header_shusei

--- js/base.js
+++ js/base.js
@@ -1,12 +1,12 @@
 /*https://coco-factory.jp/ugokuweb/move01/5-1-5/から引用*/
 //スクロールすると上部に固定させるための設定を関数でまとめる
 function FixedAnime() {
-    var headerH = $('#header').outerHeight(true);
+    var headerH = 1000;
     var scroll = $(window).scrollTop();
     if (scroll >= headerH){//headerの高さ以上になったら
-            $('#header').addClass('fixed');//fixedというクラス名を付与
+            $('#rel_header').addClass('is-show');//fixedというクラス名を付与
         }else{//それ以外は
-            $('#header').removeClass('fixed');//fixedというクラス名を除去
+            $('#rel_header').removeClass('is-show');//fixedというクラス名を除去
         }
 }
 
@@ -37,18 +37,18 @@ function animation(){
     });
 }
 
-$(window).on('load scroll',function (){
-    animation()
+$(window).on('load',function (){//ページがロードされたら
+    animation()//アニメーション実行
     setTimeout(() => {
-        $('.animation_before1').addClass('active')
+        $('.animation_before1').addClass('active')//1000ms後に表示
     }, 1000)
     setTimeout(() => {
-        $('.animation_before2').addClass('active')
+        $('.animation_before2').addClass('active')//1200ms後に表示
     }, 1200)
     setTimeout(() => {
-        $('.animation_before3').addClass('active')
+        $('.animation_before3').addClass('active')//1500ms後に表示
     }, 1500)
     setTimeout(() => {
-        $('.animation_before4').addClass('active')
+        $('.animation_before4').addClass('active')//1900ms後に表示
     }, 1900)
 });
