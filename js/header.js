772789aa8aaf53c0f08e9f2c06417e27fd80122b
c0a20030 <c0a200302c@edu.teu.ac.jp>
2022-07-16 12:23:35 +0900

    ヘッダー試行錯誤1

--- js/header.js
+++ js/header.js
@@ -1,13 +1,10 @@
 function header(text="cat"){
-    $.ajax({
-        url:"../html/header.html",
-        cache:false,
-        success:function(html){
-            document.write(html);
-            var user = document.createElement("li")
-            user.textContent = text
-            ul1 = document.getElementById("ul")
-            ul1.appendChild(user)   
-        }
-    });
+    var reader = new FileReader()
+    html = reader.readAsText("../html/header.html")
+    html = reader.result
+    document.write(html);
+    var user = document.createElement("li")
+    user.textContent = text
+    ul1 = document.getElementById("ul")
+    l1.appendChild(user)   
 }
