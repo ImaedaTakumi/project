5def707e81b20b49340bc14f97428f3351ebc028
masaya113 <c0a2012499@edu.teu.ac.jp>
2022-07-21 22:16:46 +0900

    自分用に変更

--- js/success.js
+++ js/success.js
@@ -1,15 +1,15 @@
 // ページが読み込まれたらすぐに
 $(window).on('load', function () {
     var html = location.href; //URL取得
-    if(html == "http://192.168.229.128/project/cgi/login_success.cgi"){
+    if(html == "http://192.168.119.128/project-5/cgi/login_success.cgi"){
         setTimeout(() => {
-            window.location.href = "http://192.168.229.128/project/cgi/reservation.cgi";
+            window.location.href = "http://192.168.119.128/project-5/cgi/reservation.cgi";
         }, 3000)
     }
     else{
-        if(html == "http://192.168.229.128/project/cgi/add_success.cgi"){
+        if(html == "http://192.168.119.128/project-5/cgi/add_success.cgi"){
             setTimeout(() => {
-                window.location.href = "http://192.168.229.128/project/cgi/login.cgi";
+                window.location.href = "http://192.168.119.128/project-5/cgi/login.cgi";
             }, 3000)
         }
     }
